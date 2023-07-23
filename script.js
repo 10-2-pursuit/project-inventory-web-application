9877588123b286db0c4284ead926b831328e23b5
Ruslan Poptsov <ruslanpoptsov@Ruslans-Air.askey.com>
2023-07-23 02:34:26 -0400

    completed-lab-inventory-web-app

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('resource-form');
     const resourcesContainer = document.querySelector('.resources');
+    const removeButton = document.querySelector('.remove-btn')
+
+    
   
     // Remove resource
-    resourcesContainer.addEventListener('click', (event) => {
-      if (event.target.classList.contains('remove-btn')) {
+    removeButton.addEventListener('click', (event) => {
         event.target.parentElement.remove();
-      }
     });
   
     // Form submission
@@ -24,23 +25,23 @@ document.addEventListener('DOMContentLoaded', () => {
       const newResource = document.createElement('div');
       newResource.className = 'resource';
       newResource.innerHTML = `
-        <h3>Item ${resourcesContainer.children.length + 1}</h3>
+      <div class="resourceInfo">
+        <h3>Album ${resourcesContainer.children.length + 1}</h3>
         <p>Name: ${nameInput.value}</p>
         <p>Price: $${priceInput.value}</p>
         <p>In Stock: ${stockInput.value === 'yes' ? 'Yes' : 'No'}</p>
         <button class="remove-btn">Remove</button>
+        </div>
+        <div class="resourceImage">
+        <img src="https://i.pinimg.com/550x/18/b9/ff/18b9ffb2a8a791d50213a9d595c4dd52.jpg" alt="resource"
+        />
+      </div>
       `;
+      
   
       resourcesContainer.insertBefore(newResource, resourcesContainer.firstChild);
   
       form.reset();
-    });
-  
-    // Form reset
-    form.addEventListener('reset', () => {
-      setTimeout(() => {
-        form.reset();
-      }, 0);
     });
   });
   
